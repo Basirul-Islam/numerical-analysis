0daa7e8681cc8aec7d91346785e08a27df5558ad
Basirul-Islam <46535955+Basirul-Islam@users.noreply.github.com>
2019-03-29 00:58:36 +0600

    Update df.c

--- df.c
+++ df.c
@@ -3,23 +3,7 @@
 #define MAXCHAR 1000
 int main(void)
 {	
-	
-	/*
-	FILE *fp;
-	char str[MAXCHAR];
-	char* filename = "/home/iit/df.txt";
-	
-	
-	fp = fopen(filename,"r");
-	
-	if(fp == NULL)
-	{
-		printf("file not opened!!");
-		return 1;
-	}
-	
-	
-*/
+
 	int n;
 	
 	scanf("%d",&n);
@@ -27,36 +11,57 @@ int main(void)
 	double x[n],f[n];
 	double fdf,bdf,cdf;
 	
+	double y1[n];
+	double y2[n];
+	
 	for(int i=0;i<n;i++)
 	{
 		scanf("%lf%lf",&x[i],&f[i]);
 	}
 	
-	printf("\"x\",\"f\",\"df\"\n");
+	printf("\"x\",\"f\",\"df1\",\"df2\"\n");
 	
-	printf("%lf,%lf,%lf\n",x[0],f[0],fdf);
+	//printf("%lf,%lf,%lf\n",x[0],f[0],fdf);
 	
-	/*
-  	printf("\"x\",\"f(x)\"\n");
-	while(fgets(str,MAXCHAR, fp) != NULL)
-	printf("%s",str);
-	
-	return 0;
-	*/
-	fdf=(f[1]-f[0])/(x[1]-x[0]); 
-	//fclose(fp);
+	fdf=(f[1]-f[0])/(x[1]-x[0]);
+	y1[0] = fdf; 
 
 	for(int i=1;i<n-1;i++)
 	{
 		cdf=(f[i+1]-f[i-1])/(x[i+1]-x[i-1]);
 		
-		printf("%lf,%lf,%lf\n",x[i],f[i],cdf);
+		//printf("%lf,%lf,%lf\n",x[i],f[i],cdf);
+		
+		y1[i] = cdf;
 	}
 	
 	bdf=(f[n-1]-f[n-2])/(x[n-1]-x[n-2]);
 	
-	printf("%lf,%lf,%lf\n",x[n-1],f[n-1],bdf);
+	y1[n-1] = bdf; 
 	
+	//printf("%lf,%lf,%lf\n",x[n-1],f[n-1],bdf);
+	
+	fdf=(y1[1]-y1[0])/(x[1]-x[0]);
+	y2[0] = fdf; 
+	
+	for(int i=1;i<n-1;i++)
+	{
+	 	cdf=(y1[i+1]-y1[i-1])/(x[i+1]-x[i-1]);
+		
+		//printf("%lf,%lf,%lf\n",x[i],f[i],cdf);
+		
+		y2[i] = cdf;
+	
+	}
+	bdf=(y1[n-1]-y1[n-2])/(x[n-1]-x[n-2]);
+	
+	y2[n-1] = bdf;
+	
+	for(int i = 0; i<n; i++)
+	{
+		printf("%lf,%lf,%lf,%lf\n",x[i],f[i],y1[i],y2[i]);
+	
+	} 
 	
 	return 0;
 }
